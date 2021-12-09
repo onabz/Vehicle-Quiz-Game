ec9fe2ee041a51a23b1d58fb04489ea44830ba34
onabz <ONABOLUMBE@GMAIL.COM>
2021-12-09 08:29:43 +0000

    create structure for game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
 /* jshint esversion: 8 */
 
+function beginGame() {
+   
+}
+
+function nextQuestion() {
+
+}
+
+function getQuesiton() {
+
+}
+
+function resetState() {
+
+}
+
+function chooseAnswer() {
+
+}
+
+function gameOver() {
+
+}
+
+function bodyStatus() {
+
+}
+
+function bodyStatusRemove() {
+
+}
