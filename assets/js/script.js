0da449934dd9c13546132753faf1366e8a71d8b8
onabz <ONABOLUMBE@GMAIL.COM>
2021-12-09 09:09:27 +0000

    Add function for shuffling questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,18 @@
 
 const beginButton = document.getElementById('start-btn');
 const questionBox = document.getElementById('question-box');
+let mixQuestions, currentQuestionNumber;
 
 beginButton.addEventListener('click', beginGame);
 
 /**
- * function for initiating the game when user clicks on the 'Let's Go' button
+ * function for initiating the game when user clicks on the 'Let's Go' button,
+ * function for selecting questions at random
  */
 function beginGame() {
     beginButton.classList.add('vanish');
+    mixQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionNumber = 0
     questionBox.classList.remove('vanish');
     nextQuestion();
 }
