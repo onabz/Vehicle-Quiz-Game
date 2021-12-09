e1ba7a96fa9442c72749cb6d70edf3a1a6ae282c
onabz <ONABOLUMBE@GMAIL.COM>
2021-12-09 08:44:07 +0000

    Add event listener and function for start button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 /* jshint esversion: 8 */
 
+const beginButton = document.getElementById('start-btn');
+const questionBox = document.getElementById('question-box');
+
+beginButton.addEventListener('click', beginGame);
+
+/**
+ * function for initiating the game when user clicks on the 'Let's Go' button
+ */
 function beginGame() {
-   
+    beginButton.classList.add('vanish')
+    questionBox.classList.remove('vanish')
 }
 
 function nextQuestion() {
