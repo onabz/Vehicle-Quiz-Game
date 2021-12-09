1f8080f72c4236bca47a099590a703cd70d19684
onabz <ONABOLUMBE@GMAIL.COM>
2021-12-09 11:35:31 +0000

    Add function to show quesiton and answers and event listener for choose answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 /* jshint esversion: 8 */
 
 const beginButton = document.getElementById('start-btn');
-const questionBox = document.getElementById('question-box');
+const questionBoxItem = document.getElementById('question-box');
+const questionItem = document.getElementById('question');
+const answerItems = document.getElementById('answer-buttons');
 let mixQuestions, currentQuestionNumber;
 
 beginButton.addEventListener('click', beginGame);
@@ -12,24 +14,43 @@ beginButton.addEventListener('click', beginGame);
  */
 function beginGame() {
     beginButton.classList.add('vanish');
-    mixQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionNumber = 0
-    questionBox.classList.remove('vanish');
+    mixQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionNumber = 0;
+    questionBoxItem.classList.remove('vanish');
     nextQuestion();
 }
 
+/**
+ * function for initiating what happens when user clicks on the 'next' button
+ */
 function nextQuestion() {
-
+    getQuestion(mixQuestions[currentQuestionNumber]);
 }
 
-function getQuesiton() {
-
+/** 
+ * function for displaying questions
+*/
+function getQuestion(question) {
+    questionItem.innerText = question.question;
+    question.answers.forEach(answer => {
+        let button = document.createElement('button')
+        button.innerText = answer.option
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', chooseAnswer)
+        answerItems.appendChild(button)
+    })
 }
 
 function resetState() {
 
 }
 
+/**
+ * function for initiating what happens when a user selects an answer
+ */
 function chooseAnswer() {
 
 }
